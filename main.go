1b947f8c6ad0e0d2b7418f2e9117cdcd213dca29
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-11-29 11:08:04 +0700

    feat: add websocket pool setup and route handler

--- main.go
+++ main.go
@@ -4,16 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/IhsanAlhakim/chat-backend/internal/websocket"
 )
 
 func main() {
 	PORT := ":8080"
+
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		websocket.ServeWebsocket(pool, w, r)
+	})
+
 	fmt.Println("React Go ChatApp v1")
 	fmt.Println("Server started at Port" + PORT)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
 
 	if err := http.ListenAndServe(PORT, nil); err != nil {
 		log.Println("Shutting down server...")
